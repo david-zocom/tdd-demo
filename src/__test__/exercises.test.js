b91ef0049286720f7b6b9e6b8ac3f8946c6f2dfd
David Andersson <David Andersson>
2017-05-23 11:38:28 +0200

    Exercise 11, Counter

--- src/__test__/exercises.test.js
+++ src/__test__/exercises.test.js
@@ -1,6 +1,4 @@
-const Memory = require('../exercises.js');
-/*
-*/
+const { Memory, Counter } = require('../exercises.js');
 
 describe('exercise 10, Memory', () => {
 	test('remember saves value in class', () => {
@@ -27,10 +25,31 @@ describe('exercise 10, Memory', () => {
 	
 })
 
-
-
-
-
+/*
+Skriv testfall för metoderna countUp och countDown. countUp ska räkna upp egenskapen value med ett. countDown ska räkna ner med ett om värdet är större än noll, annars ska funktionen kasta ett Error.*/
+describe('exercise 11, Counter', () => {
+	test('countUp increases value with 1', () => {
+		let counter = new Counter();
+		counter.countUp();
+		expect( counter.value ).toBe( 2 );
+		
+		counter.countUp();
+		expect( counter.value ).toBe( 3 );
+	});
+	
+	test('countDown decreases value with 1', () => {
+		let counter = new Counter();
+		counter.countDown();
+		expect( counter.value ).toBe( 0 );
+	});
+	
+	test('countDown throws when 0', () => {
+		let counter = new Counter();
+		counter.countDown();
+		expect( () => { counter.countDown() } )
+			.toThrow('Cannot count below zero');
+	})
+})
 
 
 
--- src/exercises.js
+++ src/exercises.js
@@ -14,4 +14,20 @@ class Memory {
 	// this.data är namnet på egenskapen som sparas i klassen
 }
 
-module.exports = Memory;
+class Counter {
+	constructor() {
+		this.value = 1;
+	}
+	countUp() {
+		this.value++;
+	}
+	countDown() {
+		if( this.value === 0 )
+			throw new Error('Cannot count below zero');
+		this.value--;
+	}
+}
+
+
+
+module.exports = { Memory, Counter };
